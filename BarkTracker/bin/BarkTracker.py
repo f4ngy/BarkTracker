82b2aad1f501d62c6841319309dc3a42421e91e2
Joseph Prezioso <joeprezioso@comcast.net>
2014-03-01 12:57:36 -0500

    First shot at email code
    It should work. If not, I have ideas on what might cause problems.

--- BarkTracker/bin/BarkTracker.py
+++ BarkTracker/bin/BarkTracker.py
@@ -1,6 +1,10 @@
 import numpy
+import time
 import pyaudio
 import analyse
+import smtplib # for emailing people
+#SERVER = "localhost"
+
 
 pyaud = pyaudio.PyAudio()
 
@@ -22,6 +26,30 @@ while True:
 	
 	if analyse.loudness(samps) > -25:
 		print "Too Loud!"
+		
+		#Setup email stuff
+		FROM = "BARK TRACKER" #This might need to be an email address. We will soon se
+
+		TO = ["lle6138@rit.edu"] #must be a list
+		
+		SUBJECT = "Noisy dog"
+		
+		#Dog noisy at hour, minute, second, AM/PM
+		TEXT = "Your dog is being noisy at " + time.strftime("%I:%M%S%p")
+		
+		#prepare actual message
+		message = """\
+		From: %s
+		To: %s
+		Subject %s
+		
+		%s
+		"""%(FROM, ", ".join(TO), SUBJECT, TEXT)
+		
+		#send mail
+		server = smtplib.SMTP('myserver')
+		server.sendmail(FROM, TO, message)
+		server.quit()
 	
 	elif analyse.loudness(samps) > -30:
 		print "Normal Ambient Sound"	
