037f8c2d25f2c04793d89d700f8f1efc9b72a1f2
Lindsey <lle6138@users.noreply.github.com>
2014-03-07 14:22:37 -0500

    Adding documentation
    And removing magic numbers

--- BarkTracker/bin/BarkTracker.py
+++ BarkTracker/bin/BarkTracker.py
@@ -8,23 +8,28 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from multiprocessing import Process
 
-#The following four lines should probably be editable by the user
-gmailUser = "someGmailHere@gmail.com"   #pick an email
-gmailPassword = "gmailPasswordHere"     #enter password (shouldn't be a problem if you're using your own email address)
-recipient = "recipientEmailHere"
-subject = "Noisy dog"
-
+#The following variables should be customized by the user
+gmailUser = "someGmailHere@gmail.com"   # sender e-mail
+gmailPassword = "gmailPasswordHere"     # sender password
+recipient = "recipientEmailHere"        # recipient e-mail
+subject = "Noisy dog"                   # e-mail subject line
+streamChunk = 1024                      # chunk used for the audio input stream
+sampleRate = 48000                      # the sample rate of the user's mic
+input_device_index = 2                  # device index for the user's mic
+numChannels = 2                         # number of channels for the user's mic
+audio_format = pyaudio.paInt16          # the audio format
+ambient_db = -15                        # the ambience noise level in db
+email_timer = 15                        # number of minutes between e-mails
 
 emailSentAt = None
 pyaud = pyaudio.PyAudio()
 
 #open input stream 
 stream = pyaud.open(
-	format = pyaudio.paInt16, #16 bit
-	channels = 2, #1 = mono; 2 = stereo; I have no idea what 3+ are...
-	#rate = 44100, #at 44100 Hz
-	rate = 48000, #at 48000 Hz
-	input_device_index = 2, #whichever USB port the microphone is plugged in to
+	format = audio_format,
+	channels = numChannels, 
+	rate = sampleRate, 
+	input_device_index = input_device_index, #whichever USB port the microphone is plugged in to
 	input = True)
 	
 def sendEmail():
@@ -32,9 +37,9 @@ def sendEmail():
 	#Dog noisy at hour, minute, second, AM/PM
 	text = "Your dog is being noisy at " + currentTime.strftime("%Y-%m-%d %H:%M:%S")        #This is what shows up in the content of the email
 	message = MIMEMultipart()
-	message['Subject'] = subject    #This...should be self-explanatory: it's the subject of the email
-	message['From'] = gmailUser     #Where its being sent from (chosen by user)
-	message['To'] = recipient       #Who gets the email (or which of your multiple email addresses this is being sent to)
+	message['Subject'] = subject    # The subject of the email
+	message['From'] = gmailUser     # Where its being sent from (chosen by user)
+	message['To'] = recipient       # Who gets the e-mail
 	mimeText = MIMEText(text, 'plain')
 	message.attach(mimeText)
 	
@@ -50,12 +55,12 @@ print("Starting BarkTracker")
 	
 while True:
 	#read raw mic data
-	rawsamps = stream.read(1024)
+	rawsamps = stream.read(streamChunk)
 	#convert to NumPy array
 	samps = numpy.fromstring(rawsamps, dtype = numpy.int16)
 
-	#send email if the loudness exceeds -15 db
-	if analyse.loudness(samps) >= -15:
+	#send email if the loudness exceeds ambient noise
+	if analyse.loudness(samps) >= ambient_db:
                 #This is the time at which the sound was detected
 		currentTime = datetime.datetime.now()   
 
@@ -63,11 +68,10 @@ while True:
 		if(emailSentAt != None):
 			timeDifference = currentTime - emailSentAt 
 		else:
-			timeDifference = datetime.timedelta(minutes=100)
+			timeDifference = datetime.timedelta(minutes=email_timer + 1)
 
-		#Only send an email to the user if it has been less than 30 minutes since the last email
-			#We may want to allow the user to change this to suit his/her needs
-		if(timeDifference > datetime.timedelta(minutes=30)):
+		#Only send an email to the user if one hasn't been sent recently
+		if(timeDifference > datetime.timedelta(minutes=email_timer)):
 			print ("Sending email about dog!")      
 			emailSentAt = currentTime
 			p = Process(target=sendEmail)   #sending the email is in a process so that it won't...cause things to crash
@@ -75,6 +79,6 @@ while True:
 
 		else:
 			print ("Dog is noisy but email has already been sent")
-	#elif analyse.loudness(samps) <	 -15:
+	#elif analyse.loudness(samps) <	 ambient_db:
 		#print ("Normal Ambient Sound")	 
 	
