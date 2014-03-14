e19fd4c1cfa0471ea875635edb7d84333e5aed2d
Joseph Prezioso <joeprezioso@comcast.net>
2014-03-14 16:26:27 -0400

    Fixed typos. Added requirements.txt

--- BarkTracker/setup.py
+++ BarkTracker/setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 
 setup(name = "BarkTracker",
     version = "0.1.0",
-    description = "Bark Tracker monitors and notifies you if your dog is barking while youre away.",
+    description = "Bark Tracker monitors and notifies you if your dog is barking while you are away.",
     author = "Lindsey Ellis and Joseph Prezioso",
     author_email = "lle6138@gmail.com and joeprezioso@comcast.net",
     url = "https://github.com/lle6138/BarkTracker",
@@ -29,7 +29,7 @@ setup(name = "BarkTracker",
     the user's Raspberry Pi, Desktop, or Laptop. If your dog (or cat, bird, baby, rowdy brother-in-law, etc.)
     makes noise above a certain level of decibels, Bark Tracker sends you an email alerting you to that fact.
 
-    To avoid spamming the user with countless emails over the course of a day, Bark Tracker waits 30 minutes
+    To avoid spamming the user with countless emails over the course of a day, Bark Tracker waits 15 minutes
     (or some other amount of time, determined by the user) between emails to alert the user.
     """
     #Name the folder where your packages live:
